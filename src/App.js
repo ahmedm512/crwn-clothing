557ecc5ac7016d7b02587021a964c7cbfa3ab1f3
ahmedm512 <1600155@eng.asu.edu.eg>
2020-03-20 16:16:01 +0200

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import './App.css';
+import {Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 
+import './App.css';
+
+function HatsPage() {
+  return(
+    <div>
+    <h1>Hats Page</h1>
+    </div>
+  )
+
+}
 
 function App() {
   return (
-    <div >
-   <HomePage/>
-    </div>
+<div >
+  <Switch>
+
+  <Route exact path='/' component={HomePage} />
+  <Route path='/hats' component={HatsPage} />
+   
+  </Switch>
+</div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import tachyons from 'tachyons'
+import Tachyons from 'tachyons'
+import {BrowserRouter} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>,
+    
+    document.getElementById('root'));
  
