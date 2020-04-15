9093dd1c61a894fb332e6dd2bb0cbd8e19a40544
ahmedm512 <1600155@eng.asu.edu.eg>
2020-04-15 13:05:47 +0200

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding component with flow for directory/shop

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -10,7 +10,7 @@ const CheckoutItem = ({cartItem, clearItem, addItem, removeItem}) => {
     return (
         <div className='checkout-item'>
             <div className='image-container'>
-                <img drc={imageUrl} alt='item' />
+                <img src={imageUrl} alt='item' />
             </div>
             <span className='name'>{name}</span>
             <span className='quantity'>
